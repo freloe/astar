780031c86b6692fa3250974cddc79e293c630860
Frederick Loeffel <f.loeffel@fz-juelich.de>
2023-06-19 19:17:01 +0200

    Added Position struct

--- src/include/model.hpp
+++ src/include/model.hpp
@@ -7,3 +7,20 @@
  * 
  * @copyright Copyright (c) 2023
  */
+
+namespace {
+    struct Position {
+        int x;
+        int y;
+
+        friend bool operator==(Position& a, Position& b) {
+            return a.x == b.x && a.y == b.y;
+        }
+    };
+}
+
+class AStar {
+    private:
+
+    public:
+};
