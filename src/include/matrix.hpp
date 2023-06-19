24b74b610daae725704263adb0219bc64d9f428c
Frederick Loeffel <f.loeffel@fz-juelich.de>
2023-06-19 19:31:18 +0200

    Added Matrix data struct

---
+++ src/include/matrix.hpp
@@ -0,0 +1,31 @@
+/**
+ * @file data.hpp
+ * @author Frederick Loeffel
+ * @brief Consistent data model across different controlles, models and views
+ * @version 0.1
+ * @date 2023-06-19
+ * 
+ * @copyright Copyright (c) 2023
+ * 
+ */
+#include <array>
+
+/**
+ * @brief For consistency: \n
+ *          Walls == -1 \n
+ *          Empty ==  0 \n
+ *          Start ==  1 \n
+ *          Goal  ==  2 \n
+ * 
+ * @tparam width 
+ * @tparam height 
+ */
+template<int width,int height>
+struct Matrix {
+    private:
+        std::array<std::array<int,height>,width> data;
+    public:
+        int& operator()(int x, int y) {
+            return data.at(x).at(y);
+        }
+};
