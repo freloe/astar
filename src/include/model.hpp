999db182112a59bacf01c3ef593c447cc09ecc59
Frederick Loeffel <f.loeffel@fz-juelich.de>
2023-06-19 19:08:51 +0200

    Intialized first model, name might change later

---
+++ src/include/model.hpp
@@ -0,0 +1,9 @@
+/**
+ * @file model.hpp
+ * @author Frederick Loeffel
+ * @brief This file contains the A* Pathfinding Algorithm
+ * @version 0.1
+ * @date 2023-06-19
+ * 
+ * @copyright Copyright (c) 2023
+ */
