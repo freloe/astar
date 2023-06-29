dc1627e0771d815add794dc30b17912f9a8673cb
Frederick Loeffel <f.loeffel@fz-juelich.de>
2023-06-29 11:49:31 +0200

    Updated drawing strategy

--- src/main.cpp
+++ src/main.cpp
@@ -13,15 +13,22 @@ float heuristic(Position a, Position b){
 };
 
 int main(){
-    Matrix<17,13> m;
+    Matrix<100,100> m;
     AStar<heuristic> A1;
     View v(1000,1000);
     Controller<decltype(A1),decltype(v),decltype(m)> c{A1,v,m};
+    v.drawMatrix<decltype(m)>(m);
 
     while(v.isOpen()) {
         c.pollInput();
         c.pollEvents();
-        v.drawMatrix<decltype(m)>(m);
+        for(auto p : c.getChanges()) {
+            if(p.x == -1 && p.y == -1)
+                v.drawMatrix<decltype(m)>(m);
+            else
+                v.updateMatrix(m,p.x,p.y);
+        }
+        c.getChanges().clear();
         v.draw();
     }
 
