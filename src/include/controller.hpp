7352b9e8aaa0e960069bf0c301451033e87bba29
Frederick Loeffel <f.loeffel@fz-juelich.de>
2023-06-29 10:19:57 +0200

    Added Wall functionality in pollInput

--- src/include/controller.hpp
+++ src/include/controller.hpp
@@ -8,9 +8,42 @@
  * @copyright Copyright (c) 2023
  * 
  */
+#pragma once
+#include <SFML/Window/Keyboard.hpp>
+#include <SFML/Window/Mouse.hpp>
 
-
-template<typename Model, typename View>
+template<typename Model, typename View, typename Matrix>
 class Controller {
+    private:
+        Model& model;
+        View& view;
+        Matrix& data;
+    public:
 
+        Controller(Model& model, View& view, Matrix& data) : model{model}, view{view}, data{data} {
+
+        }
+
+        void pollInput() {
+            if(sf::Keyboard::isKeyPressed(sf::Keyboard::Key::W)) {
+                sf::Vector2i pos = sf::Mouse::getPosition(view.getWindow());
+                if(pos.x >= view.getWidth() || pos.y >= view.getHeight() || pos.x < 0 || pos.y < 0)
+                    return;
+
+                int width = data.getWidth();
+                int height = data.getHeight();
+
+                int boxSizeX = view.getWidth() /width;
+                int boxSizeY = view.getHeight()/height;
+
+                float offsetX = (view.getWidth()  - (boxSizeX * width))/2.0;
+                float offsetY = (view.getHeight() - (boxSizeY * height))/2.0;
+
+                int x = (pos.x-offsetX) / boxSizeX;
+                int y = (pos.y-offsetY) / boxSizeY;
+
+                data.setWall(x,y);
+            }
+                
+        }
 };
