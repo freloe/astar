cfaf6b792078408894028219162258e75c757721
Frederick Loeffel <f.loeffel@fz-juelich.de>
2023-06-29 08:30:14 +0200

    Restricted data access to predefined fucntions

--- src/include/matrix.hpp
+++ src/include/matrix.hpp
@@ -1,5 +1,5 @@
 /**
- * @file data.hpp
+ * @file matrix.hpp
  * @author Frederick Loeffel
  * @brief Consistent data model across different controlles, models and views
  * @version 0.1
@@ -27,7 +27,9 @@ struct Matrix {
         size_t w = width,h = height;
         std::array<std::array<int,height>,width> data;
     public:
-        int& operator()(int x, int y) {
+        int xStart = 0, yStart = 0, xGoal = 1, yGoal = 0;
+        
+        int operator()(int x, int y) {
             return data.at(x).at(y);
         }
         bool notWall(int x, int y) {
@@ -39,5 +41,27 @@ struct Matrix {
 
         size_t getHeight() {
             return h;
+        }
+
+        void setStart(int x, int y) {
+            data.at(xStart).at(yStart) = 0;
+            data.at(x).at(y) = 1;
+            xStart = x;
+            yStart = y;
+        }
+
+        void setGoal(int x, int y) {
+            data.at(xGoal).at(yGoal) = 0;
+            data.at(x).at(y) = 2;
+            xGoal = x;
+            yGoal = y;
+        }
+
+        void setWall(int x, int y) {
+            data.at(x).at(y) = -1;
+        }
+
+        void setEmpty(int x, int y) {
+            data.at(x).at(y) = 0;
         }
 };
