5c280c2e83a36053aa082c9971c381b9a768f73f
Frederick Loeffel <f.loeffel@fz-juelich.de>
2023-06-29 09:39:35 +0200

    Init controller.hpp

---
+++ src/include/controller.hpp
@@ -0,0 +1,16 @@
+/**
+ * @file controller.hpp
+ * @author Frederick Loeffel
+ * @brief This file handles user input and passes that to model and view
+ * @version 0.1
+ * @date 2023-06-19
+ * 
+ * @copyright Copyright (c) 2023
+ * 
+ */
+
+
+template<typename Model, typename View>
+class Controller {
+
+};
