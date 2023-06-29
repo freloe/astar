a9477e34f5d487a9c2a38947fda299c77e1a6107
Frederick Loeffel <f.loeffel@fz-juelich.de>
2023-06-29 08:31:39 +0200

    Init view.hpp

---
+++ src/include/view.hpp
@@ -0,0 +1,19 @@
+
+/**
+ * @file view.hpp
+ * @author Frederick Loeffel
+ * @brief This file contains the necessary framework for viewing the program
+ * @version 0.1
+ * @date 2023-06-19
+ * 
+ * @copyright Copyright (c) 2023
+ * 
+ */
+#include <SFML/Graphics.hpp>
+
+template<typename Data>
+class View {
+    
+
+
+};
